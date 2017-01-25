d9ba18aac20de79bab3727310af1508324818a7f
woogerboy21 <woogerboy21@gmail.com>
2017-01-24 22:33:40 -0500

    Advanced along development. Added new functions. Created more effecient versions of existing functions.

--- servatrice/src/skynet.cpp
+++ servatrice/src/skynet.cpp
@@ -3,6 +3,7 @@
 #include <QDebug>
 #include <QSqlDatabase>
 #include <QSqlQuery>
+#include <QSqlError>
 
 Skynet::Skynet()
 {
@@ -16,60 +17,57 @@ Skynet::~Skynet()
 
 void Skynet::JudgementDay()
 {
-	initDatabase(); // create initial db connection
+    // create initial db connection
+	initDatabase();
 
-    if (doesTableExist(prepareTableName("{prefix}_user_behavior")) ) // check if db table exists and remove it to prepare for current use
-		deleteBehaviorDatabaseTable();
+    // remove old stale data table content
+    if (doesTableExist(prepareTableName("{prefix}_user_behavior")))
+        executeSimpleQuery("drop table {prefix}_user_behavior");
 
-   if (createBehaviorDatabaseTable()) // create memory table for use
+    // create new table
+    if (executeSimpleQuery("CREATE TABLE IF NOT EXISTS `{prefix}_user_behavior` (`id` int(7) unsigned zerofill NOT NULL auto_increment,`name` varchar(35),`kicks` int(1) DEFAULT 0,`badwords` int(1) DEFAULT 0,PRIMARY KEY(`id`)) ENGINE = MEMORY;"))
         qDebug() << "Judgement day has arrived";
+
+    if (addUserToBehaviorTable("testme",1,2))
+        qDebug() << setColumnValue("badwords", "testme", 50);
+
+    qDebug() << getColumnIntValue("kicks", "testme");
+    qDebug() << getColumnStringValue("name", "testme");
 }
 
-bool Skynet::addUserToBehaviorTable(const QString &user)
+bool Skynet::addUserToBehaviorTable(const QString &user, const int &kickcount, const int &badwordcount)
 {
 	qDebug() << "Adding user " << user << " to behavior table";
-	QSqlQuery query;
-	QSqlDatabase skynetDB = QSqlDatabase::database();
-	query.prepare("insert into cockatrice_user_behavior (name) values (:user_name)");
-	query.bindValue(":user_name", user);
-
-	if (query.exec())
-		return true;
-	return false;
-}
-
-bool Skynet::createBehaviorDatabaseTable()
-{
-	qDebug() << "Creating behavior database table";
-	QSqlDatabase skynetDB = QSqlDatabase::database();
-    QSqlQuery query(
-        "CREATE TABLE IF NOT EXISTS `cockatrice_user_behavior` ("
-		"`id` int(7) unsigned zerofill NOT NULL auto_increment,"
-		"`name` varchar(35),"
-        "`kicks` int(1) DEFAULT 0,"
-		"`badwords` int(1) DEFAULT 0,"
-        "PRIMARY KEY(`id`)) ENGINE = MEMORY;");
-	
-	if (query.exec())
-		return true;
-
-    return false;
-}
-
-bool Skynet::deleteBehaviorDatabaseTable()
-{
-	qDebug() << "Removing behavior database table";
-	QSqlDatabase skynetDB = QSqlDatabase::database();
-	QSqlQuery *query = prepareQuery("drop table {prefix}_user_behavior");
+    QSqlQuery *query = prepareQuery("insert into {prefix}_user_behavior (name,kicks,badwords) values (:user_name,:kick_count,:badword_count)");
+    query->bindValue(":user_name", user);
+    query->bindValue(":kick_count", kickcount);
+    query->bindValue(":badword_count", badwordcount);
 
 	if (query->exec())
 		return true;
+
+	return false;
+}
+
+bool Skynet::executeSimpleQuery(const QString &queryText)
+{
+    /*
+        SIMPLE QUERIES ARE ONES THAT DO NOT REQUIRE BINDING FOR SECURITY.
+        ONLY PASS NON-BIND QUERIES TO THIS FUNCTION FOR SECURITY REASONS.
+    */
+
+    qDebug() << "Executing query: " << queryText;
+    QSqlQuery * query = prepareQuery(queryText);
+    if (query->exec())
+        return true;
 
     return false;
 }
 
 bool Skynet::doesTableExist(const QString &table)
 {
+    /* TABLE NAME PREPERATION EXPECTED TO BE PERFORMED BY CALLER BY USING prepareTableName FUNCTION */
+
 	qDebug() << "Checking if table " << table << " exists";
 	QSqlDatabase skynetDB = QSqlDatabase::database();
     if (skynetDB.tables().contains(table))
@@ -104,4 +102,68 @@ void Skynet::initDatabase()
 	skynetDB.setUserName(settingsCache->value("database/user").toString());
 	skynetDB.setPassword(settingsCache->value("database/password").toString());
 	skynetDB.open();
+}
+
+bool Skynet::setColumnValue(const QString &column, const QString &user, const int &amount)
+{
+    QString queryText = QString("update {prefix}_user_behavior set %1 = :amount where name = :user_name").arg(column);
+    QSqlQuery * query = prepareQuery(queryText);
+    query->bindValue(":column", column);
+    query->bindValue(":amount", amount);
+    query->bindValue(":user_name", user);
+
+    if (query->exec())
+        return true;
+
+    return false;
+}
+
+bool Skynet::increaseColumnValue(const QString &column, const QString &user, const int &amount)
+{
+    QString queryText = QString("update {prefix}_user_behavior set %1 = %1+:amount where name = :user_name").arg(column);
+    QSqlQuery * query = prepareQuery(queryText);
+    query->bindValue(":amount", amount);
+    query->bindValue(":user_name", user);
+
+    if (query->exec())
+        return true;
+
+    return false;
+}
+
+bool Skynet::decreaseColumnValue(const QString &column, const QString &user, const int &amount)
+{
+    QString queryText = QString("update {prefix}_user_behavior set %1 = %1-:amount where name = :user_name").arg(column);
+    QSqlQuery * query = prepareQuery(queryText);
+    query->bindValue(":amount", amount);
+    query->bindValue(":user_name", user);
+
+    if (query->exec())
+        return true;
+
+    return false;
+}
+
+int Skynet::getColumnIntValue(const QString &column, const QString &user)
+{
+    //TODO: FIGURE OUT WHY BINDING ISNT WORKING HERE
+    QString queryText = QString("select %1 from {prefix}_user_behavior where name = '%2'").arg(column).arg(user);
+    queryText = prepareTableName(queryText);
+    QSqlQuery query(queryText);
+    if (query.first())
+        return query.value(column).toInt();
+
+    return 999;
+}
+
+QString Skynet::getColumnStringValue(const QString &column, const QString &user)
+{
+    //TODO: FIGURE OUT WHY BINDING ISNT WORKING HERE
+    QString queryText = QString("select %1 from cockatrice_user_behavior where name = '%2'").arg(column).arg(user);
+    queryText = prepareTableName(queryText);
+    QSqlQuery query(queryText);;
+    if (query.first()) {
+        return query.value(column).toString();
+    }
+    return "";
 }
--- servatrice/src/skynet.h
+++ servatrice/src/skynet.h
@@ -14,10 +14,14 @@ public:
 private:
 	QSqlQuery * prepareQuery(const QString &queryText);
 	QString prepareTableName(const QString &table);
-    bool createBehaviorDatabaseTable();
-    bool deleteBehaviorDatabaseTable();
     bool doesTableExist(const QString &table);
-	bool addUserToBehaviorTable(const QString &user);
+	bool addUserToBehaviorTable(const QString &user, const int &kickcount, const int &badwordcount);
+    bool executeSimpleQuery(const QString &queryText);
+    bool setColumnValue(const QString &column, const QString &user, const int &amount);
+    bool increaseColumnValue(const QString &column, const QString &user, const int &amount);
+    bool decreaseColumnValue(const QString &column, const QString &user, const int &amount);
+    int getColumnIntValue(const QString &column, const QString &user);
+    QString getColumnStringValue(const QString &column, const QString &user);
 	void initDatabase();
 };
 
